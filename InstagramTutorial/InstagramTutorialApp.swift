31d23e36d40213228ed310547b218dfdb8ca4029
Djordje3002 <djkartaljevic@gmail.com>
2025-06-22 16:56:50 +0200

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 22. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramTutorialTests/InstagramTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialTests.swift
+//  InstagramTutorialTests
+//
+//  Created by Djordje on 22. 6. 2025..
+//
+
+import Testing
+@testable import InstagramTutorial
+
+struct InstagramTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
