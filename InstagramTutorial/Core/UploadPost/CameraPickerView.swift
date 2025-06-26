910facea03fc65b7357ea6dc1ffa4f562dea49d1
Djordje3002 <djkartaljevic@gmail.com>
2025-06-26 10:11:21 +0200

    Camera functionality added to upploadPostView

---
+++ InstagramTutorial/Core/UploadPost/CameraPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPickerView.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 26. 6. 2025..
+//
+
+import SwiftUI
+
+struct CameraPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPickerView()
+}
