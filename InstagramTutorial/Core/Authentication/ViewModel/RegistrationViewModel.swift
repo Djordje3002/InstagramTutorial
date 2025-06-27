a31f93c853e659727064a80534ad2e104a5b04b8
Djordje3002 <djkartaljevic@gmail.com>
2025-06-27 18:20:44 +0200

    RegistrationViewModel

--- InstagramTutorial/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ InstagramTutorial/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class RegistrationViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var email: String = ""
+    @Published var password: String = ""
+}
