1b89f8621fb261afe9127f2c82fba5581bc870cc
Djordje3002 <djkartaljevic@gmail.com>
2025-06-28 22:46:47 +0200

    EditProfileView UI

---
+++ InstagramTutorial/Core/Profile/View/EditProFileRowView.swift
@@ -0,0 +1,18 @@
+//
+//  EditprofileRowView.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 28. 6. 2025..
+//
+
+import SwiftUI
+
+struct EditprofileRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditprofileRowView()
+}
---
+++ InstagramTutorial/Core/Profile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 28. 6. 2025..
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
