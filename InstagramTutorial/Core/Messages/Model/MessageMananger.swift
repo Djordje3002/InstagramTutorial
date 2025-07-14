4a44c018869ee68178e4030bc154fef99ddccd08
Djordje3002 <djkartaljevic@gmail.com>
2025-07-14 21:30:44 +0200

    Sending messages implementation

---
+++ InstagramTutorial/Core/Messages/Model/MessageMananger.swift
@@ -0,0 +1,8 @@
+//
+//  MessageMananger.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 14. 7. 2025..
+//
+
+import Foundation
---
+++ InstagramTutorial/Core/Messages/View/MeesageBubble.swift
@@ -0,0 +1,18 @@
+//
+//  MeesageBubble.swift
+//  InstagramTutorial
+//
+//  Created by Djordje on 14. 7. 2025..
+//
+
+import SwiftUI
+
+struct MeesageBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeesageBubble()
+}
